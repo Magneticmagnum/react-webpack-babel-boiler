54c672efc314f0700174b41acdea4cdb43ead2b7
magneticmagnum <noreemag@gmail.com>
2016-03-08 21:46:02 -0600

    removed the accidentally added node_modules folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
-var HTMLWebpackPlugin = require('html-webpack-plugin')
-var HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
-	template: __dirname + '/app/index.html',
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var HTMLWebpackPluginConfig = new HtmlWebpackPlugin	({
+	template: __dirname	 + '/app/index.html',
 	filename: 'index.html',
 	inject: 'body'
 });
@@ -8,14 +8,14 @@ module.exports = {
 	entry: [
 		'./app/index.js'
 	],
-	module: {
+	output: {
+		path: __dirname + '/dist',
+		filename: "index_bundle.js"
+	},
+	module:{
 		loaders: [
 			{test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
 		]
 	},
-	output: {
-		filename: "index_bundle.js",
-		path: __dirname + '/dist'
-	},
 	plugins: [HTMLWebpackPluginConfig]
-};
+}
